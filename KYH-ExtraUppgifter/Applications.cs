7436deb867d878167eb440cbcd0fab312189ab31
Christoffer S <christoffer.sladin@hotmail.com>
2022-10-01 13:06:57 +0200

    Add project files.

---
+++ KYH-ExtraUppgifter/Applications.cs
@@ -0,0 +1,36 @@
+﻿public class Applications
+{
+    public void RandomAIChatbot()
+    {
+        while (true)
+        {
+            Console.WriteLine("Skriv något till boten\n");
+            var input = Console.ReadLine();
+
+            var answer = AnswerFromBot(input);
+            Console.WriteLine(answer);
+        }
+    }
+
+    private string AnswerFromBot(string input)
+    {
+        string[] answers = new[]
+        {"Jag mår bra",
+        "Lämna mig ifred",
+        "Vad sa du?",
+        "whatever",
+        "Kanske det",
+        "Ingen aning",
+        "Ja vad bra"};
+
+        Random randomAnswer = new Random();
+        int index = randomAnswer.Next(answers.Length);
+
+        return $"{answers[index]}";
+    }
+
+    public void Run()
+    {
+        RandomAIChatbot();
+    }
+}
